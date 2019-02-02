bda6bfce32eacf4148ce1b62b03a2492a2aab117
bisfor <boyce2evan@gmail.com>
2019-02-02 08:43:44 +0700

    partial view

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,22 @@
+<div class="menu_section">
+    <h3>General</h3>
+    <ul class="nav side-menu">
+        <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+                <li><a href="#">Dashboard</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
+<div class="menu_section">
+    <h3>Administrator</h3>
+    <ul class="nav side-menu">
+        <li><a><i class="fa fa-home"></i> Management User <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+                <li><a href="{{ route('register') }}"><i class="fa fa-laptop"></i> Registration</a></li>
+                
+            </ul>
+        </li>
+    </ul>
+</div>
